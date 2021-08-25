5532a3bd8775f05dbf99f4065209368afa1016ce
Vanya01 <good.boy.2k19@gmail.com>
2021-08-25 17:22:29 +0300

    Portfolio_one_update_25.08

--- src/Header/Header_bottom.js
+++ src/Header/Header_bottom.js
@@ -1,14 +1,25 @@
 import './Header_bottom.css'
-import 'imgs/logo.png'
+import logo from '../Header/imgs/logo.png'
+import search from '../Header/imgs/Search Icon@1X.png'
+import basket from  '../Header/imgs/Cart Icon@1X.png'
 
 export function Header_bottom(){
     return(
-        <div className={'header_upper'}>
-          <div className="header_upper_top">
-               <div className="header_upper_logo">
-                   <img src={''} alt="logo"/>
+        <div className={'header_upper container'}>
+          <div className="header_upper_top d-flex a-center ">
+               <div className="header_upper_logo d-flex a-center">
+                   <img src={logo} alt="logo"/>
+                   <h1>BACK DOWN SOUTH</h1>
                </div>
-              <h1>BACK DOWN SOUTH</h1>
+                    <div className={'d-flex a-center'}>
+              <form className={'form_header'}>
+
+                  <input type="text" placeholder='Enter key words' className={'text_input'}/>
+                  <button className={'btn_top'}><img src={search} alt="search"/></button>
+              </form>
+                  <img src={basket} alt="basket"/>
+                    </div>
+
           </div>
             <div className="header_upper_bottom">
 
